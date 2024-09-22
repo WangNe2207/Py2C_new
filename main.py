56f27b8b8e4aa69e75bef56164d3aefa8754cda7
Vo Van Quang <94778939+WangNe2207@users.noreply.github.com>
2024-09-22 14:42:04 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from Py2C import Py2C
+import tensorflow as tf
+# path_w= ["Conv.cc", "Conv.hh", "Pool.cc", "Pool.hh", "Dense.cc", "CNN.hh"]
+# print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+# print(tf.__version__)
+pyc_lib = Py2C("best_loss_checkpoint.h5")
+pyc_lib.convert2C()
+pyc_lib.WriteCfile()
+# pyc_lib.del_one_file("CNN.hh")
+# pyc_lib.del_any_file(path_w)
+# pyc_lib.del_all_file()
+# pyc_lib.set_Fxp_Param((16,6))
+# pyc_lib.Write_IEEE754_32bits_Weights_File()
+# pyc_lib.Write_Float_Weights_File()
+# pyc_lib.Write_FixedPoint_Weights_File()
+pyc_lib.Write_Converted_Float_Weights_File()
